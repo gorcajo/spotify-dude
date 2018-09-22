cf1531ad18c2b3b77b47c6e908ce51cc7658b872
Guillermo OG <mr.guiller@gmail.com>
2018-09-22 21:01:54 +0200

    variables renaming

--- src/dude.py
+++ src/dude.py
@@ -30,29 +30,29 @@ class Dude(object):
 
         there_were_changes = False
 
-        stored_playlists = self.db.get_all_playlists()
+        playlists = self.db.get_all_playlists()
 
-        for stored_playlist in stored_playlists:
+        for playlist in playlists:
             try:
-                songs = self.spotify.get_all_songs_from_playlist(stored_playlist)
-                current_name = self.spotify.get_playlist_name_from_id(stored_playlist)
+                spotify_songs = self.spotify.get_all_songs_from_playlist(playlist)
+                current_name = self.spotify.get_playlist_name_from_id(playlist)
 
-                if stored_playlist.name != current_name:
-                    stored_playlist.name = current_name
-                    self.db.update_playlist_name(stored_playlist, current_name)
+                if playlist.name != current_name:
+                    playlist.name = current_name
+                    self.db.update_playlist_name(playlist, current_name)
                     self.logger.debug(f"The name of the playlist has changed to [{current_name}]")
                 else:
                     self.logger.debug("No change in playlist name")
 
-                if stored_playlist.songs_last_seen == len(songs):
+                if playlist.songs_last_seen == len(spotify_songs):
                     self.logger.debug("There are no song changes, playlist skipped")
-                elif stored_playlist.songs_last_seen < len(songs):
-                    self.logger.debug(f"Somebody added songs (before: {stored_playlist.songs_last_seen}, now: {len(songs)}), an update will be made")
-                    self._update_with_added_song(stored_playlist, songs[-1], len(songs))
+                elif playlist.songs_last_seen < len(spotify_songs):
+                    self.logger.debug(f"Somebody added songs (before: {playlist.songs_last_seen}, now: {len(spotify_songs)}), an update will be made")
+                    self._update_with_added_song(playlist, spotify_songs[-1], len(spotify_songs))
                     there_were_changes = True
                 else:
-                    self.logger.debug(f"Somebody deleted songs (before: {stored_playlist.songs_last_seen}, now: {len(songs)}), an update will be made")
-                    self._update_with_deleted_song(stored_playlist, len(songs))
+                    self.logger.debug(f"Somebody deleted songs (before: {playlist.songs_last_seen}, now: {len(spotify_songs)}), an update will be made")
+                    self._update_with_deleted_song(playlist, len(spotify_songs))
                     there_were_changes = True
 
             except:
@@ -90,11 +90,11 @@ class Dude(object):
                 self.logger.warn(f"Exception happened:\n{traceback.format_exc()}")
     
 
-    def _update_with_added_song(self, stored_playlist: Playlist, last_song: dict, current_song_count: int):
+    def _update_with_added_song(self, playlist: Playlist, last_spotify_song: dict, current_song_count: int):
         self.logger.debug(f"Retrieving the last song added along with its data")
 
-        adder = self.db.find_user_by_spotify_id(last_song["added_by"]["id"])
-        song = Song(last_song, adder)
+        adder = self.db.find_user_by_spotify_id(last_spotify_song["added_by"]["id"])
+        song = Song(last_spotify_song, adder)
 
         self.logger.debug(f"Last song was [{song}]")
 
@@ -108,10 +108,10 @@ class Dude(object):
 
         self.logger.debug(f"Next random adder: [{next_adder.name}]")
 
-        self.mailer.add_new_event_as_new_song(stored_playlist, adder, song, next_adder)
+        self.mailer.add_new_event_as_new_song(playlist, adder, song, next_adder)
 
         if not self.debug_mode:
-            self.db.update_playlist_songs(stored_playlist, current_song_count)
+            self.db.update_playlist_songs(playlist, current_song_count)
     
 
     def _update_with_deleted_song(self, stored_playlist: Playlist, current_song_count: int):
